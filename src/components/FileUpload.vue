98322994e24088441c2d84a8604ff88ba73d76ae
philipotieno <tmitchellb007@gmail.com>
2020-02-11 04:45:57 +0300

    feat: adds file upload component

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="file-upload">
+        <h3>
+            Upload File
+        </h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FileUpload',
+    components:{
+
+    }
+}
+</script>
+<style scoped>
+.file-upload{
+    text-align: center
+}
+
+</style>
