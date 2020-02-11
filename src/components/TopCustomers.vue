f578c1695868c71b0b3e3a99a20528a72bfb0c4c
philipotieno <tmitchellb007@gmail.com>
2020-02-11 04:44:12 +0300

    feat: adds top customers component

---
+++ src/components/TopCustomers.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="hello">
+    <h3>Top Customers</h3>
+      <bar-chart
+        :library="{
+          yAxis: {
+            labels: {
+              enabled: true
+            },
+            className: 'highcharts-color-0',
+            min: 0,
+            tickInterval: 2
+          },
+          plotOptions: {
+            column: {
+              borderRadius: '10px'
+            }
+          },
+          styledMode: true,
+          colors: ['#405262']
+        }"
+        :data="newArray"
+        xtitle="Name"
+        ytitle="Amount"
+      ></bar-chart>
+  </div>
+</template>
+
+<script>
+// import {mapState} from 'vuex';
+export default {
+  name: 'TopCustomers',
+  props: {
+  },
+  computed:{
+    topCustomerList(){
+        return this.$store.getters.topCustomerList;
+    },
+    newArray(){
+         return this.topCustomerList.map(obj => Object.values(obj))
+         
+     }
+  },
+ mounted(){
+    this.$store.dispatch('loadTopCustomers')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
