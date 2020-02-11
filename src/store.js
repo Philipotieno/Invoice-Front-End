064673855ed8d9ec2a83bfb565a26330830fa7e5
philipotieno <tmitchellb007@gmail.com>
2020-02-11 04:47:19 +0300

    feat: adds vuex to manage state

---
+++ src/store.js
@@ -0,0 +1,79 @@
+/* eslint-disable no-console */
+import Vue from 'vue';
+import vuex from 'vuex';
+import axios from 'axios';
+
+
+Vue.use(vuex, axios);
+
+
+export default new vuex.Store({
+    state: {
+        topCustomerList: [],
+        invoiceSummary: [],
+        postInvoices: []
+    },
+    getters: {
+        topCustomerList: state => {
+            return state.topCustomerList;
+        },
+        invoiceSummary: state => {
+            return state.invoiceSummary;
+        },
+        postInvoices: state => {
+            return state.postInvoices;
+        }
+    },
+    mutations: {
+        ADD_CUSTOMERS(state, customers) {
+            state.topCustomerList = customers
+        },
+        ADD_INVOICE_SUMMARY(state, invoice) {
+            state.invoiceSummary = invoice
+        },
+        POST_INVOICES(state, invoices) {
+            state.postInvoices = invoices
+        }
+
+    },
+    actions: {
+        loadTopCustomers({ commit }) {
+            axios
+                .get('https://philip-invoice.herokuapp.com/invoices/topcustomers')
+                .then(data => {
+                    console.log(data.data.invoices);
+                    let topCustomers = data.data.invoices;
+                    commit('ADD_CUSTOMERS', topCustomers);
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+        loadInvoiceSummary({ commit }) {
+            axios
+                .get('https://philip-invoice.herokuapp.com/invoices/summary')
+                .then(data => {
+                    console.log(data.data.Invoice);
+                    let invoiceSummary = data.data.Invoice;
+                    commit('ADD_INVOICE_SUMMARY', invoiceSummary);
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        },
+        postInvoiceDate({ commit }, params) {
+            console.log('params',params);
+            axios
+                .post('https://philip-invoice.herokuapp.com/invoices/transactions',params)
+                .then(data => {
+                    console.log("post", data.data.invoices);
+                    let invoices = data.data.invoices;
+                    commit('POST_INVOICES', invoices);
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+
+    }
+})
